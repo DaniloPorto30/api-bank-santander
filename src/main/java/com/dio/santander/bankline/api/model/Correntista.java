12d097364da376b8558e238e5961d80ca0112647
DaniloPorto30 <dprosa23@gmail.com>
2022-06-16 15:25:19 -0300

    mapeando as entidades jpa

---
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -0,0 +1,50 @@
+package com.dio.santander.bankline.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista")
+public class Correntista {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(length = 20)
+	private String cpf;
+	
+	@Column(length = 60)
+	private String nome;
+	
+	private Conta conta;
+	public Conta getConta() {
+		return conta;
+	}
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
---
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -0,0 +1,53 @@
+package com.dio.santander.bankline.api.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_movimentacao")
+public class Movimentacao {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private LocalDateTime dataHora;
+  private String descricao;
+  private Double valor;
+  private MovimentacaoTipo tipo;
+public Integer getId() {
+	return id;
+}
+public LocalDateTime getDataHora() {
+	return dataHora;
+}
+public String getDescricao() {
+	return descricao;
+}
+public Double getValor() {
+	return valor;
+}
+public MovimentacaoTipo getTipo() {
+	return tipo;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public void setDataHora(LocalDateTime dataHora) {
+	this.dataHora = dataHora;
+}
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+public void setValor(Double valor) {
+	this.valor = valor;
+}
+public void setTipo(MovimentacaoTipo tipo) {
+	this.tipo = tipo;
+}
+  
+  
+}
